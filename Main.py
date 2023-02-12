62995253eadf3f26e3c114ebe9987c49c5d11400
name <email@yandex.ru>
2023-02-12 17:01:59 +0300

    commit seven arrows movement added

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ from MapReturner import ImageGenerator
 
 FPS = 60
 interface = pygame.sprite.Group()
+SCALE = 100_000
+HALF_SCALE = SCALE / 2
 
 
 class Picture(pygame.sprite.Sprite):
@@ -33,9 +35,10 @@ def main():
 
     generator = ImageGenerator()
     picture = Picture(pygame.image.load(
-        generator.get_from_toponym(toponym, str(delta / 100_000))[1]
+        generator.get_from_toponym(toponym, str(delta / SCALE))[1]
     ))
     x, y = generator.get_position()
+    pressed_button = None
     redraw = False
     running = True
     while running:
@@ -49,13 +52,42 @@ def main():
                 elif event.key == pygame.K_PAGEDOWN:
                     delta = min(max(delta / 2, 12.5), 6_553_600)
                     redraw = True
+                elif event.key == pygame.K_RIGHT:
+                    x = float(x) + delta / HALF_SCALE
+                    if x >= 180:
+                        x = delta / HALF_SCALE
+                    x = str(x)
+                    redraw = True
+                elif event.key == pygame.K_LEFT:
+                    x = float(x) - delta / HALF_SCALE
+                    if x <= 0:
+                        x = 180 - delta / HALF_SCALE
+                    x = str(x)
+                    redraw = True
+                elif event.key == pygame.K_UP:
+                    y = str(float(y) + delta / HALF_SCALE)
+                    pressed_button = 'up'
+                    redraw = True
+                elif event.key == pygame.K_DOWN:
+                    y = str(float(y) - delta / HALF_SCALE)
+                    pressed_button = 'down'
+                    redraw = True
                 elif event.key == pygame.K_r:
                     redraw = True
         if redraw:
             print("\rДельта:", delta, end="")
-            picture.set_picture(pygame.image.load(
-                generator.get_from_cords(x, y, str(delta / 100_000))[1]
-            ))
+            try:
+                picture.set_picture(pygame.image.load(
+                    generator.get_from_cords(x, y,
+                                             str(delta / SCALE))[1]))
+            except pygame.error:
+                if pressed_button == 'up':
+                    y = str(float(y) - delta / HALF_SCALE)
+                else:
+                    y = str(float(y) + delta / HALF_SCALE)
+                picture.set_picture(pygame.image.load(
+                    generator.get_from_cords(x, y,
+                                             str(delta / SCALE))[1]))
             redraw = False
         screen.fill(pygame.Color("black"))
         interface.draw(screen)
