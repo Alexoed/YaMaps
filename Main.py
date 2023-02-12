7f6d00d236e506225b58474a657a2615dd0f6a37
name <email@yandex.ru>
2023-02-12 17:11:41 +0300

    commit eight movement speed toggle added

--- Main.py
+++ Main.py
@@ -38,6 +38,8 @@ def main():
         generator.get_from_toponym(toponym, str(delta / SCALE))[1]
     ))
     x, y = generator.get_position()
+    movement_delta = HALF_SCALE
+    mov_slow = False
     pressed_button = None
     redraw = False
     running = True
@@ -53,38 +55,47 @@ def main():
                     delta = min(max(delta / 2, 12.5), 6_553_600)
                     redraw = True
                 elif event.key == pygame.K_RIGHT:
-                    x = float(x) + delta / HALF_SCALE
+                    x = float(x) + delta / movement_delta
                     if x >= 180:
-                        x = delta / HALF_SCALE
+                        x = delta / movement_delta
                     x = str(x)
                     redraw = True
                 elif event.key == pygame.K_LEFT:
-                    x = float(x) - delta / HALF_SCALE
+                    x = float(x) - delta / movement_delta
                     if x <= 0:
-                        x = 180 - delta / HALF_SCALE
+                        x = 180 - delta / movement_delta
                     x = str(x)
                     redraw = True
                 elif event.key == pygame.K_UP:
-                    y = str(float(y) + delta / HALF_SCALE)
+                    y = str(float(y) + delta / movement_delta)
                     pressed_button = 'up'
                     redraw = True
                 elif event.key == pygame.K_DOWN:
-                    y = str(float(y) - delta / HALF_SCALE)
+                    y = str(float(y) - delta / movement_delta)
                     pressed_button = 'down'
                     redraw = True
                 elif event.key == pygame.K_r:
                     redraw = True
+                elif event.key == pygame.K_f:
+                    mov_slow = not mov_slow
+                    if mov_slow:
+                        movement_delta = SCALE
+                    else:
+                        movement_delta = HALF_SCALE
+                    print(f"\rДельта: {delta}; Медленнее: {mov_slow}",
+                          end="")
         if redraw:
-            print("\rДельта:", delta, end="")
+            print(f"\rДельта: {delta}; Медленнее: {mov_slow}",
+                  end="")
             try:
                 picture.set_picture(pygame.image.load(
                     generator.get_from_cords(x, y,
                                              str(delta / SCALE))[1]))
             except pygame.error:
                 if pressed_button == 'up':
-                    y = str(float(y) - delta / HALF_SCALE)
+                    y = str(float(y) - delta / movement_delta)
                 else:
-                    y = str(float(y) + delta / HALF_SCALE)
+                    y = str(float(y) + delta / movement_delta)
                 picture.set_picture(pygame.image.load(
                     generator.get_from_cords(x, y,
                                              str(delta / SCALE))[1]))
